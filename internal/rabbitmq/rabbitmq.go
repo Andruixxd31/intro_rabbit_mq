fd744ee380ffbc95d4f85fbddf802ae917a1def2
adnruixxd31 <andruix93@gmail.com>
2024-01-09 10:21:27 -0600

    feat: publishing mssgs to queue

--- internal/rabbitmq/rabbitmq.go
+++ internal/rabbitmq/rabbitmq.go
@@ -1,7 +1,9 @@
 package rabbitmq
 
 import (
+	"context"
 	"fmt"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -15,12 +17,48 @@ type RabbitMQ struct {
 
 func (rmq *RabbitMQ) Connect() error {
 	fmt.Println("Connecting to rabbitmq")
-	var err error
-	rmq.Conn, err = amqp.Dial("amqp://guest:guest@localhost:5672/")
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
 		return err
 	}
+	defer conn.Close()
+
 	fmt.Println("Succesfullly connected to rabbitmq")
+
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+
+	fmt.Println(q)
+
+	err = ch.PublishWithContext(
+		context.Background(),
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World!"),
+		},
+	)
+
 	return nil
 }
 
