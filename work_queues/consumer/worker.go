d823d7754f44edfb304cc80a597dc9dc1e0fb627
adnruixxd31 <andruix93@gmail.com>
2024-01-10 12:49:24 -0600

    feat: new example

---
+++ work_queues/consumer/worker.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"time"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s %s", err, msg)
+	}
+}
+
+func main() {
+
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect Rabbitmq")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to create channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"working-queue",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to declare queue")
+
+	err = ch.Qos(
+		1,
+		0,
+		false,
+	)
+	failOnError(err, "Failed to set Qos")
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to register consumer")
+
+	forever := make(chan struct{})
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+			dotCount := bytes.Count(d.Body, []byte("."))
+			t := time.Duration(dotCount)
+			time.Sleep(t * time.Second)
+			log.Printf("Done")
+		}
+
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
